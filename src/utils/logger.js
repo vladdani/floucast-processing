90dd8e49cc836cd485186293771139916200943f
Serebrennikov Stanislav <goodsmileduck@gmail.com>
2025-09-04 12:37:52 +0800

    cleanup production logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,20 +1,27 @@
 const winston = require('winston');
 
 function createLogger() {
+  // Check if running in production (ECS/CloudWatch doesn't support colors)
+  const isProduction = process.env.NODE_ENV === 'production';
+  
+  // Build format array conditionally
+  const consoleFormats = [
+    winston.format.timestamp(),
+    // Only add colorize in non-production environments
+    !isProduction && winston.format.colorize(),
+    winston.format.printf(({ timestamp, level, message, ...meta }) => {
+      let log = `${timestamp} [${level}] ${message}`;
+      if (Object.keys(meta).length > 0) {
+        log += ` ${JSON.stringify(meta)}`;
+      }
+      return log;
+    })
+  ].filter(Boolean); // Remove false values from colorize condition
+  
   // Simple console transport - ECS handles CloudWatch integration automatically
   const transports = [
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message, ...meta }) => {
-          let log = `${timestamp} [${level}] ${message}`;
-          if (Object.keys(meta).length > 0) {
-            log += ` ${JSON.stringify(meta)}`;
-          }
-          return log;
-        })
-      )
+      format: winston.format.combine(...consoleFormats)
     })
   ];
   
@@ -30,21 +37,20 @@ function createLogger() {
   });
   
   // Handle uncaught exceptions and rejections
+  const exceptionFormats = [
+    !isProduction && winston.format.colorize(),
+    winston.format.simple()
+  ].filter(Boolean);
+  
   logger.exceptions.handle(
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      )
+      format: winston.format.combine(...exceptionFormats)
     })
   );
   
   logger.rejections.handle(
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      )
+      format: winston.format.combine(...exceptionFormats)
     })
   );
   
